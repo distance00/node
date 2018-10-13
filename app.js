178a77f6f2ad9f226ce6af03ec1f2646054f8cbb
distance00 <distance00@users.noreply.github.com>
2018-10-14 03:53:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-var express = require('express');
-var app = express();
+const http = require('http');
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+const port = = process.env.PORT || 5000;
+
+const cert = `-----BEGIN CERTIFICATE-----
+MIIB5zCCAY2gAwIBAgIJAPI1efFUKZP1MAoGCCqGSM49BAMCMGAxCzAJBgNVBAYT
+AkNOMQwwCgYDVQQIDANkZCAxDDAKBgNVBAcMA2NjIDETMBEGA1UECgwKX0NhX3Jv
+b3RfIDEMMAoGA1UECwwDYmQgMRIwEAYDVQQDDAlfQ2Ffcm9vdF8wHhcNMTcxMjEw
+MTU1NDEzWhcNNDUwNDI2MTU1NDEzWjBgMQswCQYDVQQGEwJDTjEMMAoGA1UECAwD
+ZGQgMQwwCgYDVQQHDANjYyAxEzARBgNVBAoMCl9DYV9yb290XyAxDDAKBgNVBAsM
+A2JkIDESMBAGA1UEAwwJX0NhX3Jvb3RfMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcD
+QgAEAzOXnsQnIFH4BJUaz9JPG10g72wzgksUl94Fitu58v7QZnVArXA3UcmWpZcN
+T/vWtaDgzp7eHKwWkP62bzEJq6MwMC4wEQYJYIZIAYb4QgEBBAQDAgZAMAwGA1Ud
+EwQFMAMBAf8wCwYDVR0PBAQDAgIEMAoGCCqGSM49BAMCA0gAMEUCIEj7lmY447Ne
+YErlo/x2cFL/Z4c5yEYJh35IaqtHON24AiEA6WN2X6M5eW2L++ucYekWY5DHz2mc
+GqQh2l81vwFI7t0=
+-----END CERTIFICATE-----`;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'application/x-x509-ca-cert');
+	res.end(cert);
 });
 
-app.listen(3000, function () {
-  console.log('app is listening at port 3000');
+server.listen(port, () => {
+	console.log(`Server running at http://127.0.0.1:${port}/`);
 });
