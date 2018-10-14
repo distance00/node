e819b8802702dbb032aa1cf22ce96e555345bb4f
distance00 <distance00@users.noreply.github.com>
2018-10-14 11:37:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 'use strict';
 
 var http = require('http');
-var fs = require('fs');
+var cert = `-----BEGIN CERTIFICATE-----
+MIIB5zCCAY2gAwIBAgIJAPI1efFUKZP1MAoGCCqGSM49BAMCMGAxCzAJBgNVBAYT
+AkNOMQwwCgYDVQQIDANkZCAxDDAKBgNVBAcMA2NjIDETMBEGA1UECgwKX0NhX3Jv
+b3RfIDEMMAoGA1UECwwDYmQgMRIwEAYDVQQDDAlfQ2Ffcm9vdF8wHhcNMTcxMjEw
+MTU1NDEzWhcNNDUwNDI2MTU1NDEzWjBgMQswCQYDVQQGEwJDTjEMMAoGA1UECAwD
+ZGQgMQwwCgYDVQQHDANjYyAxEzARBgNVBAoMCl9DYV9yb290XyAxDDAKBgNVBAsM
+A2JkIDESMBAGA1UEAwwJX0NhX3Jvb3RfMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcD
+QgAEAzOXnsQnIFH4BJUaz9JPG10g72wzgksUl94Fitu58v7QZnVArXA3UcmWpZcN
+T/vWtaDgzp7eHKwWkP62bzEJq6MwMC4wEQYJYIZIAYb4QgEBBAQDAgZAMAwGA1Ud
+EwQFMAMBAf8wCwYDVR0PBAQDAgIEMAoGCCqGSM49BAMCA0gAMEUCIEj7lmY447Ne
+YErlo/x2cFL/Z4c5yEYJh35IaqtHON24AiEA6WN2X6M5eW2L++ucYekWY5DHz2mc
+GqQh2l81vwFI7t0=
+-----END CERTIFICATE-----`;
 
-var server = http.createServer((request, response) => {
-    console.log(request.method + ': ' + request.url);
-    if (request.method === 'GET') {
-        if (request.url === '/favicon.ico') {
-            fs.createReadStream('./favicon.ico').pipe(response);
-        } else {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            fs.createReadStream('./404.html').pipe(response);
-        }
-    }
+var server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'application/x-x509-ca-cert');
+	res.end(cert);
 });
 
 var serverPort = process.env.PORT || 5000;
